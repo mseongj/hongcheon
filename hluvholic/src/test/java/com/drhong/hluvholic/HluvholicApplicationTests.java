1a7bb259152224e35c4d1916eecd094f382715fc
Changhoon Park <102115848+pch4947@users.noreply.github.com>
2024-10-16 14:38:01 +0000

    add Backend

---
+++ hluvholic/src/test/java/com/drhong/hluvholic/HluvholicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drhong.hluvholic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HluvholicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
